110ca2d8ced22d471dd9676ec20ebf5d3efc9f43
Matheus Lopes Duarte <131881614+matheuslopesduarte@users.noreply.github.com>
2023-11-14 13:10:59 -0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+$hostname = 'database.hythelis.me';  
+$username = 'topicos'; 
+$password = 'Senha123@';
+$database = 'topicos'; 
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+echo "<script>console.log('Conexão bem-sucedida!')</script>";
+
+?>
