2a36693bebe44f7a2b063afd053bdbdffe28bc91
matheuslopesduarte <matheuslopesduarte7@gmail.com>
2023-11-27 22:16:42 -0300

    lol

---
+++ mostraproduto.php
@@ -0,0 +1,20 @@
+<?php
+
+    include ("banco.php");
+
+    $querysidebar = "SELECT * FROM produtos";
+    $resultsidebar = $conn->query($querysidebar);
+
+    if ($resultsidebar->num_rows > 0) {
+
+      $currentTopic = "";
+      while ($rowsidebar = $resultsidebar->fetch_assoc()) {
+
+        
+      echo '<img style="height:20px;" class="sidebar-img" alt="' . $rowsidebar['title'] . '" src=""></img>';
+
+      }
+    } else {
+        echo 'tem nada';
+    }
+    ?>
