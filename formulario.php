0baebad804eda2d1c6a0dd0a4cad6d6038e1c2e3
matheuslopesduarte <matheuslopesduarte7@gmail.com>
2023-11-28 12:12:51 -0300

    lolol

--- formulario.php
+++ formulario.php
@@ -1,53 +1,53 @@
 <?php
-    include ("banco.php");
+        include ("banco.php");
 
-    session_start();
-
-    if(isset( $_SESSION["adm"]) && $_SESSION["adm"] == "true"){
-
-        if(isset($_POST["type"]) && $_POST['type'] == 'criarproduto'){
-            $nome = $_POST["nome"];
-            $descricao = $_POST["descricao"];
-            $preco = $_POST['preco'];
-            $id = uniqid();
-
-            $imagem = file_get_contents($_FILES['imagem']['tmp_name']);
-            $imageData = base64_encode($imagem);
-
-            $conn->query('INSERT INTO produtos (id, nome, descricao, preco, foto, tipoid) VALUES ("'.$id.'","'.$nome.'","'.$descricao.'",'.$preco.',"'.$imageData.'","12")');
+        session_start();
     
+        if(isset( $_SESSION["adm"]) && $_SESSION["adm"] == "true"){
+    
+            if(isset($_POST["type"]) && $_POST['type'] == 'criarproduto'){
+                $nome = $_POST["nome"];
+                $descricao = $_POST["descricao"];
+                $preco = $_POST['preco'];
+                $id = uniqid();
+    
+                $imagem = file_get_contents($_FILES['imagem']['tmp_name']);
+                $imageData = base64_encode($imagem);
+    
+                $conn->query('INSERT INTO produtos (id, nome, descricao, preco, foto, tipoid) VALUES ("'.$id.'","'.$nome.'","'.$descricao.'",'.$preco.',"'.$imageData.'","12")');
+    
+            }
+        
+    ?>
+            
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="POST">
+            <input type="hidden" name="type" value="criarproduto">
+            <label for="nome">Nome</label>
+            <input id="nome" name="nome" type="text" required><br>
+            <label for="descricao">Descricao</label>
+            <textarea id="descricao" name="descricao" type="text" required></textarea><br>
+            <label for="imagem">imagem</label>
+            <input type="file" name="imagem" id="imagem" accept="image/*" required><br>
+            <label for="preco">preço</label>
+            $<input type="number" name="preco" id="preco" required><br>
+            <button type="submit">Enviar</button>
+        </form>
+    </body>
+    
+    </html>
+    
+    <?php 
+        }
+        else {
+            echo '<script>location = "login.php"</script>';
         }
     
-?>
-        
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="POST">
-        <input type="hidden" name="type" value="criarproduto">
-        <label for="nome">Nome</label>
-        <input id="nome" name="nome" type="text" required><br>
-        <label for="descricao">Descricao</label>
-        <textarea id="descricao" name="descricao" type="text" required></textarea><br>
-        <label for="imagem">imagem</label>
-        <input type="file" name="imagem" id="imagem" accept="image/*" required><br>
-        <label for="preco">preço</label>
-        $<input type="number" name="preco" id="preco" required><br>
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-
-</html>
-
-<?php 
-    }
-    else {
-        echo '<script>location = "login.php"</script>';
-    }
-
-?>
+    ?>
